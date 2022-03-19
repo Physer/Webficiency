bde11afc5dc55302785d3e130e2600bbcef8da06
Alex Schouls <alex_schouls@live.com>
2022-03-19 14:56:54 +0100

    Added an example repository method

--- backend/src/Business/Repository.cs
+++ backend/src/Business/Repository.cs
@@ -1,11 +1,25 @@
-﻿namespace Business;
+﻿using AutoMapper;
+using Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Business;
 
 public class Repository : IRepository
 {
+    private readonly IMapper _mapper;
+    private readonly WebficiencyContext _webficiencyContext;
 
+    public Repository(WebficiencyContext webficiencyContext, 
+        IMapper mapper)
+    {
+        _webficiencyContext = webficiencyContext;
+        _mapper = mapper;
+    }
+
+    public IEnumerable<T> GetAll<T>() where T : class, new() => _webficiencyContext.Set<T>().AsNoTracking().ToList();
 }
 
 public interface IRepository
 {
-
+    IEnumerable<T> GetAll<T>() where T : class, new();
 }
